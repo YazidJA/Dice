26df11a1f3a5e88558f939be2cd6abbc408ffe0c
Yazid J. Alaoui <yazid.ja@gmail.com>
2021-03-17 14:15:13 +0100

    First release

---
+++ script.js
@@ -0,0 +1,12 @@
+const dice1 = document.querySelector("#img1");
+const dice2 = document.querySelector("#img2");
+const btn = document.querySelector("button");
+
+function roll() {
+    let randomNumber1 = parseInt(Math.random() * 6) + 1;
+    let randomNumber2 = parseInt(Math.random() * 6) + 1;
+    dice1.src = `images/dice${randomNumber1}.png`;
+    dice2.src = `images/dice${randomNumber2}.png`;
+}
+
+btn.addEventListener("click", roll)
