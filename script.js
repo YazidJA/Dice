e414fae93bf4431fdbbb4e2808c3cd4cfdbbe637
Yazid J. Alaoui <yazid.ja@gmail.com>
2021-03-28 03:56:32 +0100

    Math.floor

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const dice2 = document.querySelector("#img2");
 const btn = document.querySelector("button");
 
 function roll() {
-    let randomNumber1 = parseInt(Math.random() * 6) + 1;
-    let randomNumber2 = parseInt(Math.random() * 6) + 1;
+    let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+    let randomNumber2 = Math.floor(Math.random() * 6) + 1;
     dice1.src = `images/dice${randomNumber1}.png`;
     dice2.src = `images/dice${randomNumber2}.png`;
 }
