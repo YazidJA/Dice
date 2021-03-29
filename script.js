611a54f8947ce0b7f124f5259981d12798e53182
Yazid J. Alaoui <yazid.ja@gmail.com>
2021-03-30 00:20:20 +0100

    Disable Zoom

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ const dice2 = document.querySelector("#img2");
 const btn = document.querySelector("button");
 
 function roll() {
-    let randomNumber1 = Math.floor(Math.random() * 6) + 1;
-    let randomNumber2 = Math.floor(Math.random() * 6) + 1;
-    dice1.src = `images/dice${randomNumber1}.png`;
-    dice2.src = `images/dice${randomNumber2}.png`;
+  let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+  let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+  dice1.src = `images/dice${randomNumber1}.png`;
+  dice2.src = `images/dice${randomNumber2}.png`;
 }
 
-btn.addEventListener("click", roll)
+btn.addEventListener("click", roll);
